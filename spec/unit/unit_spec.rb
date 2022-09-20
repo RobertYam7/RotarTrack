5f479b105a305a8221f7757cd68fe4177957046c
root <root@localhost>
2022-09-20 04:48:52 +0000

    added unit test case for event creation, check date format

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -45,6 +45,52 @@ RSpec.describe Account, type: :model do
     subject.Email = nil
     expect(subject).not_to be_valid
   end
+
+  RSpec.describe Event, type: :model do
+
+    subject do
+      described_class.new(Title: 'midnight yell', Description: "yell at midnight", Date: '01/01/20001', Time: '12:00 AM', Location: "9123456780", Points: 10);
+    end
+    
+    it 'is valid with all valid attributes' do
+      expect(subject).to be_valid
+    end
+  
+    it 'is not valid without a Title' do
+      subject.Title = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a Description' do
+      subject.Description = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a Date' do
+      subject.Date = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a Time' do
+      subject.Time = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a Location' do
+      subject.Location = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a Points' do
+      subject.Points = nil
+      expect(subject).not_to be_valid
+    end
+
+  end
+    
+
+
+  
   # it 'is not valid without a status id' do
   #   subject.status_id = nil
   #   expect(subject).not_to be_valid
